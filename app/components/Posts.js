93e45df71a1256a0afb94b5dc3f0ce94f11693ef
Chris Norwood <cnorwood92@gmail.com>
2019-09-20 16:29:31 -0700

    Refactor Posts to use hooks

--- app/components/Posts.js
+++ app/components/Posts.js
@@ -4,51 +4,53 @@ import { fetchMainPosts } from '../utils/api'
 import Loading from './Loading'
 import PostsList from './PostsList'
 
-export default class Posts extends React.Component {
-  state = {
-    posts: null,
-    error: null,
-    loading: true,
-  }
-  componentDidMount() {
-    this.handleFetch()
-  }
-  componentDidUpdate(prevProps) {
-    if (prevProps.type !== this.props.type) {
-      this.handleFetch()
-    }
-  }
-  handleFetch () {
-    this.setState({
+function postsReducer(state, action) {
+  if (action.type === 'fetch') {
+    return {
       posts: null,
       error: null,
       loading: true
-    })
-
-    fetchMainPosts(this.props.type)
-      .then((posts) => this.setState({
-        posts,
-        loading: false,
-        error: null
-      }))
-      .catch(({ message }) => this.setState({
-        error: message,
-        loading: false
-      }))
-  }
-  render() {
-    const { posts, error, loading } = this.state
-
-    if (loading === true) {
-      return <Loading />
     }
-
-    if (error) {
-      return <p className='center-text error'>{error}</p>
+  } else if (action.type === 'success') {
+    return {
+      posts: action.posts,
+      error: null,
+      loading: false
     }
-
-    return <PostsList posts={posts} />
+  } else if (action.type === 'error') {
+    return {
+      posts: state.posts,
+      error: action.error.message,
+      loading: false
+    }
+  } else {
+    throw new Error(`This action type isn't supported.`)
   }
+}
+
+export default function Posts ({ type }) {
+  const [state, dispatch] = React.useReducer(
+    postsReducer,
+    { posts: null, error: null, loading: true }
+  )
+
+  React.useEffect(() => {
+    dispatch({ type: 'fetch' })
+
+    fetchMainPosts(type)
+      .then(posts => dispatch({ type: 'success', posts }))
+      .catch(error => dipatch({ type: 'error', error }))
+  }, [type])
+
+  if (state.loading === true) {
+    return <Loading />
+  }
+
+  if (state.error) {
+    return <p className='center-text error'>{state.error}</p>
+  }
+
+  return <PostsList posts={state.posts} />
 }
 
 Posts.propTypes = {
